c7ebe2606c6ea49f3067ea58d20fede001a5dc16
EJAJ <105592400+WebDevEJAJ@users.noreply.github.com>
2026-03-15 00:37:41 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL || 'https://task-management-steel-five.vercel.app',
     credentials: true,
   })
 );
